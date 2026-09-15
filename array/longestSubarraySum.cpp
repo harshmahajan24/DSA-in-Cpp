544f77dfa8af760788bccef6901c454a181dfcb7
Harsh Mahajan <aneliteharsh@gmail.com>
2026-09-15 11:02:18 +0530

    added optimal solution of longest subarray with sum target

--- array/longestSubarraySum.cpp
+++ array/longestSubarraySum.cpp
@@ -4,7 +4,8 @@ using namespace std;
 class Solution
 {
 public:
-    int longestSubarray(int arr[], int n, int target)
+    // brute force approach:
+    int longestSubarrayBrute(int arr[], int n, int target)
 
     {
         int length = 0;
@@ -26,16 +27,39 @@ public:
         return length;
     }
 
-    
+    int longestSubarrayOptimal(int arr[], int n, int target)
+    {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int length = 0;
+
+        while (right < n)
+        {
+            sum += arr[right];
+            while (left <= right && sum > target)
+            {
+                sum = sum - arr[left];
+                left += 1;
+            }
+
+            if (sum == target)
+            {
+                length = max(length, right - left + 1);
+            }
+            right++;
+        }
+        return length;
+    }
 };
 
 int main()
 {
-    int a[3] = {-1, 1, 1};
+    int a[5] = {2, 1, -1, -1, 2};
     int k = 4;
 
     Solution solution;
-    int len = solution.longestSubarray(a, 3, k);
+    int len = solution.longestSubarrayOptimal(a, 5, k);
 
     cout << "The length of the longest subarray is: " << len << "\n";
     return 0;
